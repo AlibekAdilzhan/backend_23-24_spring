3d70d9d7164683baffb33d4315ef10a46c103cb1
AlibekAdilzhan <alibek.adilzhanov@gmail.com>
2024-03-28 08:58:12 +0500

    week 12 v1

--- library/books/views.py
+++ library/books/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from books.models import Book
 
 
 def books(request):
-    return HttpResponse("Hello world")
+    template = loader.get_template("all_books.html")
+    all_books = Book.objects.all().values()
+    my_context = {
+        "all_books": all_books
+    }
+    return HttpResponse(template.render(context=my_context))
