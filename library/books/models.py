5a399e1c49017918b40505ad33087c29b2c0496c
AlibekAdilzhan <alibek.adilzhanov@gmail.com>
2024-03-28 10:20:47 +0500

    week 11 v2

--- library/books/models.py
+++ library/books/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=128)
     author = models.CharField(max_length=128)
+    year = models.DateField(null=True)
+    price = models.IntegerField(null=True)
--- library/books/views.py
+++ library/books/views.py
@@ -6,8 +6,16 @@ from books.models import Book
 
 def books(request):
     template = loader.get_template("all_books.html")
-    all_books = Book.objects.all().values()
+    all_books = Book.objects.all()
     my_context = {
         "all_books": all_books
     }
     return HttpResponse(template.render(context=my_context))
+
+def description(request, id):
+    book = Book.objects.get(id=id)
+    template = loader.get_template("description.html")
+    context = {
+        "mybook": book
+    }
+    return HttpResponse(template.render(context=context))
